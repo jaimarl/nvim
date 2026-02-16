9abedfee13cb21a0b24939ea1f0103c1eec2a5f5
jaimarl <jaimarl.me@gmail.com>
2026-02-16 23:33:53 +0300

    Initial commit

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }: {
-options.apps.neovim.enable = lib.mkEnableOption "Neovim";
-
-config = lib.mkIf config.apps.neovim.enable {
-
-    home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../nvim;
-
-};}
